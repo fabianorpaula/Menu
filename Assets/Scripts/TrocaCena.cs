525e173f4d3bde853cdb2502eed04bead19d8e57
fabianorpaula <fabiano.rpaula@gmail.com>
2024-11-12 21:28:17 -0300

    Informações

--- Assets/Scripts/TrocaCena.cs
+++ Assets/Scripts/TrocaCena.cs
@@ -9,13 +9,13 @@ public class TrocaCena : MonoBehaviour
 
     public void ChamarCena()
     {
-
+        //Chamar pelo nome por extenso
         SceneManager.LoadScene("Fase1");
     }
 
     public void ChamaFase(int numerofase)
     {
-
+        //Chamar pela Ordem no Build
         SceneManager.LoadScene(numerofase);
 
         
